803b9b70eee412218220d5694cc3beba0d195cb2
Dann <techmemo@gmail.com>
2010-10-20 22:15:54 +0900

    initial commit

---
+++ lib/DBIx/Simple/DataSection.pm
@@ -0,0 +1,47 @@
+package DBIx::Simple::DataSection;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::Simple::DataSection -
+
+=head1 SYNOPSIS
+
+  use DBIx::Simple::DataSection;
+
+=head1 DESCRIPTION
+
+DBIx::Simple::DataSection is
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+  http://github.com/dann/
+
+=head1 CONTRIBUTORS
+
+Many thanks to:
+
+
+=head1 AUTHOR
+
+dann E<lt>techmemo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
